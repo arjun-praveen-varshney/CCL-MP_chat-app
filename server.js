e2ae7de1cc8c1bd7d4ec594d15e4c11d7258e342
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2025-10-06 00:36:26 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
